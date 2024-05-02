b528ee3beb55e17981d8e903b04412e9a573a4af
Sihab Hossain <sihabhossain380@gmail.com>
2024-05-02 10:30:17 +0600

    video 11 done

---
+++ module1/src/1.11.ts
@@ -0,0 +1,42 @@
+const age: number = 19;
+
+if (age >= 18) {
+  console.log("Adult");
+} else {
+  console.log("not adult");
+}
+
+const isAdult = age >= 18 ? "Adult" : "Not adult";
+
+console.log({ isAdult });
+
+const isAuthenticated = null;
+
+const result1 = isAuthenticated ?? "guest1";
+const result2 = isAuthenticated ? isAuthenticated : "guest2";
+
+console.log({ result1 }, { result2 });
+
+type UserDetails = {
+  name: string;
+  address: {
+    city: string;
+    road: string;
+    presentAddress: string;
+    permanentAddress?: string;
+  };
+};
+
+const UserDetails = {
+  name: "sihab",
+  address: {
+    city: "dhaka",
+    road: "gulshan2",
+    presentAddress: "baridhara",
+  },
+};
+
+const Address =
+  UserDetails?.address?.permanentAddress ?? "no permanent address";
+
+console.log({ Address });
